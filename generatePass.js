f2a7ce4a1d4b3557b9eab4a6edbf8dfa0b4b56da
Masum Reza <67681547+rimasumdev@users.noreply.github.com>
2024-09-01 06:39:30 +0000

    upload

---
+++ generatePass.js
@@ -0,0 +1,31 @@
+const myObj = {
+  name: 'masum',
+  birthYear: 1990,
+  siteName: 'google',
+};
+function password(obj) {
+  if (typeof obj !== 'object' || obj === null || Object.keys(obj).length == 0) {
+    console.log('Invalid Data');
+  } else {
+    const { name, birthYear, siteName } = obj;
+    if (
+      typeof name === 'string' &&
+      name !== '' &&
+      typeof birthYear === 'number' &&
+      typeof siteName === 'string' &&
+      siteName !== '' &&
+      birthYear.toString().length == 4
+    ) {
+      const user = name.charAt(0).toUpperCase() + name.slice(1);
+      const year = birthYear.toString();
+      const site = siteName.charAt(0).toUpperCase() + siteName.slice(1);
+      const password = site + '#' + user + '@' + year;
+      return password;
+    } else {
+      return 'Some Information is Missing To Generate Password!';
+    }
+  }
+}
+
+const pass = password(myObj);
+console.log(pass);
